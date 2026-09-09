60c178448ed60247564ac50afb38215f578da68b
Rauf Salamzade <salamzader@gmail.com>
2026-09-09 16:34:35 -0700

    Update bofasa commands to use '--auto' flag

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,5 @@ cd test_case/
 
 # run bofasa on test set of four Staphylococcus genomes
 # Assumes bofasa setup is already run!!!
-bofasa prep -i Genomes/*.fna -o Bofasa_Prep_Results/ -c 4 -auto
-bofasa run -i Bofasa_Prep_Results/ -o Bofasa_Run_Results/ -c 4 -auto
+bofasa prep -i Genomes/*.fna -o Bofasa_Prep_Results/ -c 4 --auto
+bofasa run -i Bofasa_Prep_Results/ -o Bofasa_Run_Results/ -c 4 --auto
